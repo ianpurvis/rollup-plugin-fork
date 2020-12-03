145a2f43bc61a9374b9c8d19f64fbb60e72c22f2
Ian Purvis <ian@purvisresearch.com>
2020-12-02 18:59:13 -0600

    Let example workers live after run
      Avoids issues when debugging with Safari web inspector

--- examples/complex-web-worker/src/index.js
+++ examples/complex-web-worker/src/index.js
@@ -4,7 +4,6 @@ const worker = new Worker(workerUrl)
 
 worker.onmessage = ({ data }) => {
   document.body.innerText = JSON.stringify(data, null, 4)
-  worker.terminate()
 
   // Expose data for puppeteer:
   window._data = data
