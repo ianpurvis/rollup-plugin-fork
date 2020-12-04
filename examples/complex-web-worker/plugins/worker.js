6cfdfa807c83b80724bb99e76a2d26f8b1d31057
Ian Purvis <ian@purvisresearch.com>
2020-12-04 17:23:14 -0600

    Resolve worker urls from location
      Fixes relative paths between chunk and asset

--- examples/complex-web-worker/plugins/worker.js
+++ examples/complex-web-worker/plugins/worker.js
@@ -1,10 +1,10 @@
-// Example of a minimal plugin to support web workers.  Resolves file urls
-// against `WindowOrWorkerGlobalScope.origin` and ensures they are absolute for
-// `WindowOrWorkerGlobalScope.fetch`
+// Example of a minimal plugin to support web workers.
+// Resolves file names against `location` and transforms them into absolute
+// urls for compatibility with `WindowOrWorkerGlobalScope.fetch`
 export default ({ include }) => ({
   name: 'worker',
   resolveFileUrl({ chunkId, fileName }) {
     if (!include.test(chunkId)) return
-    return `new URL(${JSON.stringify(fileName)}, origin).href`
+    return `new URL(${JSON.stringify(fileName)}, location).href`
   }
 })
