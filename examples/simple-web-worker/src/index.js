6a26cfe9feef26334c606cd0b8c344978a33a837
Ian Purvis <ian@purvisresearch.com>
2020-12-04 20:45:36 -0600

    Adds simple worker example test

--- examples/simple-web-worker/src/index.js
+++ examples/simple-web-worker/src/index.js
@@ -1,3 +1,8 @@
 import workerUrl from './worker.js'
 
-new Worker(workerUrl).onmessage = ({ data }) => document.body.append(data)
+new Worker(workerUrl).onmessage = ({ data }) => {
+  document.body.innerHTML = data
+
+  // Expose data for puppeteer:
+  window._data = data
+}
