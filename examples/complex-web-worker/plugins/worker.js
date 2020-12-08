d36311d418e0d1d2846f54ecba6c87b5e12c7b0b
Ian Purvis <ian@purvisresearch.com>
2020-12-07 21:48:35 -0600

    Resolve complex worker urls with relative path

--- examples/complex-web-worker/plugins/worker.js
+++ examples/complex-web-worker/plugins/worker.js
@@ -1,9 +1,9 @@
 // Example of a minimal plugin to support web workers.
-// Resolves file names against `location` and transforms them into absolute
+// Resolves file paths against `location` and transforms them into absolute
 // urls for compatibility with `WindowOrWorkerGlobalScope.fetch`
 export default () => ({
   name: 'worker',
-  resolveFileUrl({ fileName }) {
-    return `new URL(${JSON.stringify(fileName)}, location).href`
+  resolveFileUrl({ relativePath }) {
+    return `new URL(${JSON.stringify(relativePath)}, location).href`
   }
 })
